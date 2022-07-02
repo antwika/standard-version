e1068d74953bba6ccf5490b1eca46416b72bc46a
Anton Wiklund <ixuz07@gmail.com>
2022-07-02 22:14:02 +0200

    chore: remove unused

--- bin/cli.js
+++ bin/cli.js
@@ -8,8 +8,6 @@ const cmdParser = require('../dist/command').default;
 if (process.version.match(/v(\d+)\./)[1] < 6) {
   console.error('standard-version: Node v6 or greater is required. `standard-version` did not run.');
 } else {
-  // const standardVersion = require('../dist/index');
-  // const cmdParser = require('../dist/command');
   standardVersion(cmdParser.argv)
     .catch((err) => {
       console.log('Error:', err);
