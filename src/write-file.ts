5892e8e76d2fddd13dbc009396fb9c77fe0335b1
Anton Wiklund <ixuz07@gmail.com>
2022-07-03 20:16:39 +0200

    test: write-file

--- src/write-file.ts
+++ src/write-file.ts
@@ -1,6 +1,12 @@
 import fs from 'fs';
 
-const writeFile = (args: any, filePath: string, content: string) => {
+// TODO: This type is incomplete and just types a subset of its properties.
+type WriteFileArgs = {
+  dryRun?: boolean,
+  [key: string]: any;
+};
+
+const writeFile = (args: WriteFileArgs, filePath: string, content: string) => {
   if (args.dryRun) return;
   fs.writeFileSync(filePath, content, 'utf8');
 };
---
+++ test/unit/write-file.test.ts
@@ -0,0 +1,22 @@
+import fs from 'fs';
+import writeFile from '../../src/write-file';
+
+jest.mock('fs');
+
+describe('write-file', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('does not write to any file if argument "dryRun" is set to "true".', async () => {
+    const result = writeFile({ dryRun: true }, 'filepath', 'content');
+    expect(result).not.toBeDefined();
+  });
+
+  it('writes content to file if argument "dryRun" is not "true".', async () => {
+    jest.spyOn(fs, 'writeFileSync').mockImplementation();
+    const result = writeFile({ dryRun: false }, 'filepath', 'content');
+    expect(result).not.toBeDefined();
+    expect(fs.writeFileSync).toHaveBeenCalledWith('filepath', 'content', 'utf8');
+  });
+});
