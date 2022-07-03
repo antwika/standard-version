bb075c81fbdcbb3d7ce6e79fe4d11763063dcbd7
Anton Wiklund <ixuz07@gmail.com>
2022-07-03 20:09:21 +0200

    test: runLifecycleScript

--- src/run-lifecycle-script.ts
+++ src/run-lifecycle-script.ts
@@ -3,12 +3,21 @@ import figures from 'figures';
 import checkpoint from './checkpoint';
 import runExec from './run-exec';
 
-const runLifecycleScript = async (args: any, hookName: any) => {
-  const { scripts } = args;
+// TODO: This type is incomplete and just types a subset of its properties.
+type RunLifecycleScriptArgs = {
+  scripts?: Record<string, string>,
+  silent?: boolean,
+  [key: string]: any;
+};
+
+const runLifecycleScript = async (args: RunLifecycleScriptArgs, hookName: string) => {
+  const { scripts, silent } = args;
   if (!scripts || !scripts[hookName]) return undefined;
   const command = scripts[hookName];
-  checkpoint(args, 'Running lifecycle script "%s"', [hookName]);
-  checkpoint(args, '- execute command: "%s"', [command], chalk.blue(figures.info));
+  if (!silent) {
+    checkpoint({ silent: !!silent }, 'Running lifecycle script "%s"', [hookName]);
+    checkpoint({ silent: !!silent }, '- execute command: "%s"', [command], chalk.blue(figures.info));
+  }
   return runExec(args, command);
 };
 
---
+++ test/unit/run-lifecycle-script.test.ts
@@ -0,0 +1,39 @@
+import chalk from 'chalk';
+import runLifecycleScript from '../../src/run-lifecycle-script';
+import runExec from '../../src/run-exec';
+import checkpoint from '../../src/checkpoint';
+
+jest.mock('../../src/run-exec');
+jest.mock('../../src/checkpoint');
+
+describe('run-lifecycle-script', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('does not execute anything if there is no "scripts" provided in the arguments.', async () => {
+    const result = await runLifecycleScript({ silent: true }, 'hook-name');
+    expect(result).not.toBeDefined();
+  });
+
+  it('does not execute anything if "scripts" provided but the specific hook does not exist.', async () => {
+    const result = await runLifecycleScript({ silent: true, scripts: {} }, 'hook-name');
+    expect(result).not.toBeDefined();
+  });
+
+  it('does not print anything if specific hook exists but the argument "silent" is set to "true".', async () => {
+    jest.spyOn(global.console, 'error').mockImplementation();
+    await runLifecycleScript({ silent: true, scripts: { 'hook-name': 'foo -h' } }, 'hook-name');
+    expect(console.error).not.toHaveBeenCalled();
+    expect(runExec).toHaveBeenCalledWith({ scripts: { 'hook-name': 'foo -h' }, silent: true }, 'foo -h');
+  });
+
+  it('does calls the checkpoint functions to output messages if silent is "false".', async () => {
+    jest.spyOn(global.console, 'error').mockImplementation();
+    await runLifecycleScript({ silent: false, scripts: { 'hook-name': 'foo -h' } }, 'hook-name');
+    expect(console.error).not.toHaveBeenCalled();
+    expect(runExec).toHaveBeenCalledWith({ scripts: { 'hook-name': 'foo -h' }, silent: false }, 'foo -h');
+    expect(checkpoint).toHaveBeenCalledWith({ silent: false }, 'Running lifecycle script "%s"', ['hook-name']);
+    expect(checkpoint).toHaveBeenCalledWith({ silent: false }, '- execute command: "%s"', ['foo -h'], chalk.blue('i'));
+  });
+});
