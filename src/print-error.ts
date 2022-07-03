7c046981e0b021e2aa9b8b45d0e8471c548b95f0
Anton Wiklund <ixuz07@gmail.com>
2022-07-03 19:01:08 +0200

    test: print-error

--- src/print-error.ts
+++ src/print-error.ts
@@ -1,6 +1,19 @@
 import chalk from 'chalk';
 
-const printError = (args: any, msg: any, opts?: any) => {
+// TODO: This type is incomplete and just types a subset of its properties.
+type PrintErrorArgs = {
+  silent: boolean,
+  [key: string]: any;
+}
+
+// TODO: This type is incomplete and just types a subset of its properties.
+type PrintErrorOpts = {
+  level?: 'info' | 'warn' | 'error' | 'log' | 'debug',
+  color?: string,
+  [key: string]: any;
+}
+
+const printError = (args: PrintErrorArgs, msg: string, opts?: PrintErrorOpts) => {
   if (!args.silent) {
     const combined = { level: 'error', color: 'red', ...opts };
 
