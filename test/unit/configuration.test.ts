5a4ad47144b96b2187795ba5ca428de10c823b68
Anton Wiklund <ixuz07@gmail.com>
2022-07-03 16:59:31 +0200

    test: configuration

--- test/unit/configuration.test.ts
+++ test/unit/configuration.test.ts
@@ -3,9 +3,15 @@ import findUp from 'find-up';
 import fs from 'fs';
 import { getConfiguration } from '../../src/configuration';
 
+// eslint-disable-next-line no-var
+var validModule1 = jest.fn().mockReturnValue({ foo: 'bar' });
+
 jest.mock('path');
 jest.mock('find-up');
 jest.mock('fs');
+jest.mock('valid-module-1.js', () => validModule1, { virtual: true });
+jest.mock('valid-module-2.js', () => ({ foo: 'bar' }), { virtual: true });
+jest.mock('invalid-module.js', () => 123, { virtual: true });
 
 describe('configuration', () => {
   it('returns an empty object if no configuration file could be found.', () => {
@@ -22,5 +28,24 @@ describe('configuration', () => {
     const configuration = getConfiguration();
     expect(configuration).toBeDefined();
     expect(configuration).toStrictEqual({ foo: 'bar' });
+  });
+
+  it('throws an error if the resolved config is not an object or function.', () => {
+    jest.spyOn(findUp, 'sync').mockReturnValueOnce('invalid-module.js');
+    jest.spyOn(path, 'extname').mockReturnValueOnce('.js');
+    expect(() => getConfiguration()).toThrowError('[standard-version] Invalid configuration in invalid-module.js provided. Expected an object but found number.');
+  });
+
+  it('can resolve a configuration from a module that exports a default function', () => {
+    jest.spyOn(findUp, 'sync').mockReturnValueOnce('valid-module-1.js');
+    jest.spyOn(path, 'extname').mockReturnValueOnce('.js');
+    expect(getConfiguration()).toStrictEqual({ foo: 'bar' });
+    expect(validModule1).toHaveBeenCalledTimes(1);
+  });
+
+  it('can resolve a configuration from a module that exports a plain object', () => {
+    jest.spyOn(findUp, 'sync').mockReturnValueOnce('valid-module-2.js');
+    jest.spyOn(path, 'extname').mockReturnValueOnce('.js');
+    expect(getConfiguration()).toStrictEqual({ foo: 'bar' });
   });
 });
